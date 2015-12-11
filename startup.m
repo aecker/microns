d15e642d17c970e0cd08bac2f2f325420a6e2e9e
Alexander Ecker <alexander.ecker@uni-tuebingen.de>
2015-12-11 13:38:23 +0100

    startup script

---
+++ startup.m
@@ -0,0 +1,10 @@
+function startup
+
+p = fileparts(fileparts(mfilename('fullpath')));
+addpath(fullfile(p, 'jake'))
+addpath(fullfile(p, 'dimitri'))
+addpath(fullfile(p, 'commons/schemas'))
+addpath(fullfile(p, 'commons/lib'))
+addpath(fullfile(p, 'commons/visual-stimuli'))
+addpath(fullfile(p, 'vangoghstim'))
+addpath(fullfile(p, 'oopsi'))
