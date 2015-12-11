bb44e32ec2146c903b1f831c9f7343342476104e
Alexander Ecker <alexander.ecker@uni-tuebingen.de>
2015-12-11 13:38:34 +0100

    STA for van Gogh noise

---
+++ +vangogh/RFMethod.m
@@ -0,0 +1,16 @@
+%{
+vangogh.RFMethod (lookup) #   methods for computing receptive fields from noise
+rf_method :tinyint
+-----
+method :varchar(20)    # the name of the method
+%}
+
+classdef RFMethod < dj.Relvar
+    methods
+        function fill(self)
+            self.inserti({
+                1   'STA'
+                })
+        end
+    end
+end
